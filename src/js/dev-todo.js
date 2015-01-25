d7b2463712f5a1e6397915d1c3cc57e6d022ec6d
Zach Munro-Cape <zach.munrocape@gmail.com>
2015-01-25 17:50:47 -0400

    Added placeholder backstretch image

---
+++ src/js/dev-todo.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  //populateList();
+  //img_index = Math.floor((Math.random() * 8) + 1);
+  //img_url = 'http://munrocape.github.io/' + img_index + '.png'
+  img_url = '../city.jpg'
+  $.backstretch(img_url)
+});
